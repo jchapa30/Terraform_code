78b83eaf712d517836c91ceecbd1f85a4b68ef44
Joey Chapa <joeychapa@yahoo.com>
2023-08-15 09:29:06 -0500

    main and variables update

--- main.tf
+++ main.tf
@@ -7,11 +7,33 @@ resource "aws_vpc" "Development" {
   }
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "dev-subnet-1" {
   vpc_id     = aws_vpc.Development.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "Main"
+    Name = "dev-subnet-1"
   }
+}
+data "aws_vpc" "existing_vpc"{
+  default = true
+}
+
+resource "aws_subnet" "dev-subnet-2" {
+  vpc_id     = data.aws_vpc.existing_vpc.id
+  cidr_block = "172.31.48.0/20"
+  avalibility_zone = "us-east-1a"
+
+
+  tags = {
+    Name = "dev-subnet-2"
+  }
+}
+
+output "dev-vpc-id"{
+  value = aws_vpc.Development.id
+}
+
+output "dev-subnet-id"{
+  value = aws_subnet.dev-subnet-1.id
 }
