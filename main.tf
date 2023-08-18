58663d4bd3cf4d631c59a4f22ac127d6fe7beebf
Joey Chapa <joeychapa@yahoo.com>
2023-08-18 14:13:26 -0500

    updated code

--- main.tf
+++ main.tf
@@ -1,43 +1,56 @@
-resource "aws_vpc" "Development" {
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "availability_zone" {}
+variable "env_prefix" {}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_vpc" "myapp-vpc" {
   cidr_block       = var.vpc_cidr_block
   instance_tenancy = "default"
 
   tags = {
-    Name = var.environment
+    Name = "${var.env_prefix}-vpc"
   }
 }
-data "aws_vpc" "existing_vpc" {
-  default = true
-}
 
 
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id     = aws_vpc.Development.id
+resource "aws_subnet" "myapp-subnet1" {
+  vpc_id     = aws_vpc.myapp-vpc.id
   cidr_block = var.subnet_cidr_block
   default    = "10.0.10.0/24"
   type       = string
 
   tags = {
-    Name = "dev-subnet-1"
+    Name = "${var.env_prefix}-subnet-1"
   }
 }
 
-
-resource "aws_subnet" "dev-subnet-2" {
-  vpc_id            = data.aws_vpc.existing_vpc.id
-  cidr_block        = var.subnet_cidr_block2
-  availability_zone = "us-east-1a"
-  type              = string
+resource "aws_internet_gateway" "myapp-aws_internet_gateway" {
+  vpc_id = aws_vpc.myapp-vpc.id
 
   tags = {
-    Name = "dev-subnet-2"
+    Name = "${var.env_prefix}-igw"
   }
 }
 
-output "dev-vpc-id" {
-  value = aws_vpc.Development.id
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-aws_internet_gateway.id
+  }
+  tags = {
+    Name = "${var.env_prefix}-rtb"
+  }
 }
 
-output "dev-subnet-id" {
-  value = aws_subnet.dev-subnet-1.id
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id      = aws_subnet.myapp-subnet1.id
+  route_table_id = aws_route_table.myapp-route-table.id
 }
+
---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+subnet_cidr_block  = "10.0.20.0/24"
+subnet_cidr_block2 = "10.0.20.0/24"
+vpc_cidr_block     = "10.0.0.0/16"
+environment        = "development"
+
